7e6db8a1ce5004791aaeb03aee422f4ef7df52e9
Alina232323 <alina.polianska@vergesense.com>
2023-06-03 13:55:44 -0700

    adjusted chat icon

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -3,32 +3,34 @@ import "../styling/Nutrition.css"
 function Nutrition() {
     return (
       <div className="nutrition-container">
-        <h1> Nutrition page</h1>
-
+        <div className="nutr-img-container">
+        <img className="header-image-nutrition" src="https://fitfusion.s3.us-west-1.amazonaws.com/nutr-header.png" alt=""></img>
+        <h1 className="nutrition-header"> Nutrition page</h1>
+        </div>
         <div className="grid-container-nutrition">
-          <div>
-          <h1>Breakfast</h1>
+          <div className="nutr-box">
           <img src="https://fitfusion.s3.us-west-1.amazonaws.com/breakfast.png" alt=""></img>
+          <h2>Breakfast</h2>
           </div>
-          <div>
-          <h1>Lunch</h1>
+          <div className="nutr-box">
           <img src="https://fitfusion.s3.us-west-1.amazonaws.com/lunch1.png" alt=""></img>
+          <h2>Lunch</h2>
           </div>
-          <div>
-          <h1>Dinner</h1>
+          <div className="nutr-box">
           <img src="https://fitfusion.s3.us-west-1.amazonaws.com/dinner1.png" alt=""></img>
+          <h2>Dinner</h2>
           </div>
-          <div>
-          <h1>Smoothies</h1>
+          <div className="nutr-box">
           <img src="https://fitfusion.s3.us-west-1.amazonaws.com/smoothies.png" alt=""></img>
+          <h2>Smoothies</h2>
           </div>
-          <div>
-          <h1>Desserts</h1>
+          <div className="nutr-box">
           <img src="https://fitfusion.s3.us-west-1.amazonaws.com/desserts.png" alt=""></img>
+          <h2>Desserts</h2>
           </div>
-          <div>
-          <h1>Snacks</h1>
+          <div className="nutr-box">
           <img src="https://fitfusion.s3.us-west-1.amazonaws.com/snacks.png" alt=""></img>
+          <h2>Snacks</h2>
           </div>
         </div>
         
