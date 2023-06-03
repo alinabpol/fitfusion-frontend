85b78cfef2d9b9b11e8e6aa9b9470fb875635c6f
Alina232323 <alina.polianska@vergesense.com>
2023-06-02 21:31:45 -0700

    initial set up for /nutrition

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -1,12 +1,29 @@
-
+import "../styling/Nutrition.css"
 
 function Nutrition() {
     return (
       <div className="nutrition-container">
         <h1> Nutrition page</h1>
+
+        <div classname="grid-container">
+
+          <h1>Breakfast</h1>
+          <img src="" alt=""></img>
+          <h1>Lunch</h1>
+          <img src="" alt=""></img>
+          <h1>Dinner</h1>
+          <img src="" alt=""></img>
+          <h1>Smoothies</h1>
+          <img src="" alt=""></img>
+          <h1>Desserts</h1>
+          <img src="" alt=""></img>
+          <h1>Snacks</h1>
+          <img src="" alt=""></img>
+        </div>
+        
       </div>
     );
   }
   
-  export default Nutrition;
+  export default Nutrition
   
