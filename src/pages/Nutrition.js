174ea33647ec3aaad58a3a13e51009c29d8dbd0a
Alina232323 <alina.polianska@vergesense.com>
2023-06-03 12:51:22 -0700

    added images for nutrition grid

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -5,20 +5,31 @@ function Nutrition() {
       <div className="nutrition-container">
         <h1> Nutrition page</h1>
 
-        <div classname="grid-container">
-
+        <div className="grid-container-nutrition">
+          <div>
           <h1>Breakfast</h1>
-          <img src="" alt=""></img>
+          <img src="https://fitfusion.s3.us-west-1.amazonaws.com/breakfast.png" alt=""></img>
+          </div>
+          <div>
           <h1>Lunch</h1>
-          <img src="" alt=""></img>
+          <img src="https://fitfusion.s3.us-west-1.amazonaws.com/lunch1.png" alt=""></img>
+          </div>
+          <div>
           <h1>Dinner</h1>
-          <img src="" alt=""></img>
+          <img src="https://fitfusion.s3.us-west-1.amazonaws.com/dinner1.png" alt=""></img>
+          </div>
+          <div>
           <h1>Smoothies</h1>
-          <img src="" alt=""></img>
+          <img src="https://fitfusion.s3.us-west-1.amazonaws.com/smoothies.png" alt=""></img>
+          </div>
+          <div>
           <h1>Desserts</h1>
-          <img src="" alt=""></img>
+          <img src="https://fitfusion.s3.us-west-1.amazonaws.com/desserts.png" alt=""></img>
+          </div>
+          <div>
           <h1>Snacks</h1>
-          <img src="" alt=""></img>
+          <img src="https://fitfusion.s3.us-west-1.amazonaws.com/snacks.png" alt=""></img>
+          </div>
         </div>
         
       </div>
