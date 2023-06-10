f7e60a4401be788411117cf50bfee92a88c4c2b7
Alina232323 <alina.polianska@vergesense.com>
2023-06-09 21:42:08 -0700

    set up the rest of index pages for nutrition

--- src/pages/Desserts.js
+++ src/pages/Desserts.js
@@ -1,12 +1,54 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import "../styling/Nutrition.css"
 
 function Desserts() {
+
+  const [allDesserts, setAlDesserts] = useState([])
+
+  const URL = "https://fitfusion.herokuapp.com/api/v1/desserts/"
+  
+  
+    const getAllData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log("lunch data:", data.data);
+      setAlDesserts(data.data);
+    };
+  
+    useEffect(() => {
+      getAllData();
+      // eslint-disable-next-line
+    }, []);
+
+
+
     return (
-      <div className="home-container">
+      <div className="recipes-flex-container">
         <h1> Desserts page</h1>
+        <div className="grid-container-nutrition">
+        {allDesserts ? (
+          allDesserts.map((dessert) => (
+            <div key={dessert._id} className="recipes">
+
+            <div className="nutr-box">
+            <Link to={`/desserts/show/${dessert._id}`}>
+              <img className="img-recipes nutr-images" src={dessert.img} alt=""></img>
+            </Link>
+              <h2 className="recipe-titles">{dessert.title}</h2>
+          </div>
+          </div>
+
+          ))
+          ):(
+            <h1>Loading...</h1>
+            )}
+
+      </div>
 
       </div>
     );
   }
-  
   export default Desserts;
   
--- src/pages/Smoothies.js
+++ src/pages/Smoothies.js
@@ -1,12 +1,53 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import "../styling/Nutrition.css"
 
 function Smoothies() {
+
+  const [allSmoothies, setAllSmoothies] = useState([])
+
+  const URL = "https://fitfusion.herokuapp.com/api/v1/smoothies/"
+  
+  
+    const getAllData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log("lunch data:", data.data);
+      setAllSmoothies(data.data);
+    };
+  
+    useEffect(() => {
+      getAllData();
+      // eslint-disable-next-line
+    }, []);
+
+
+
     return (
-      <div className="home-container">
-        <h1> Smoothies page</h1>
+      <div className="recipes-flex-container">
+        <h1> Snacks page</h1>
+        <div className="grid-container-nutrition">
+        {allSmoothies ? (
+          allSmoothies.map((smoothie) => (
+            <div key={smoothie._id} className="recipes">
+
+            <div className="nutr-box">
+            <Link to={`/smoothies/show/${smoothie._id}`}>
+              <img className="img-recipes nutr-images" src={smoothie.img} alt=""></img>
+            </Link>
+              <h2 className="recipe-titles">{smoothie.title}</h2>
+          </div>
+          </div>
+
+          ))
+          ):(
+            <h1>Loading...</h1>
+            )}
+
+      </div>
 
       </div>
     );
   }
-  
   export default Smoothies;
-  
