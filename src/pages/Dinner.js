0422b90207302210dd4fb212dec6e198eed25fd8
Alina232323 <alina.polianska@vergesense.com>
2023-06-09 20:52:59 -0700

    lunch and dinner pages set up

--- src/pages/Dinner.js
+++ src/pages/Dinner.js
@@ -1,12 +1,53 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import "../styling/Nutrition.css"
 
 function Dinner() {
+  const [allDinner, setAllDinner] = useState([])
+
+  const URL = "https://fitfusion.herokuapp.com/api/v1/dinner/"
+  
+  
+    const getAllDinner = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log("lunch data:", data.data);
+      setAllDinner(data.data);
+    };
+  
+    useEffect(() => {
+      getAllDinner();
+      // eslint-disable-next-line
+    }, []);
+
+
+
     return (
-      <div className="home-container">
+      <div className="recipes-flex-container">
         <h1> Dinner page</h1>
+        <div className="grid-container-nutrition">
+        {allDinner ? (
+          allDinner.map((dinner) => (
+            <div key={dinner._id} className="recipes">
+
+            <div className="nutr-box">
+            <Link to={`/dinner/show/${dinner._id}`}>
+              <img className="img-recipes nutr-images" src={dinner.img} alt=""></img>
+            </Link>
+              <h2 className="recipe-titles">{dinner.title}</h2>
+          </div>
+          </div>
+
+          ))
+          ):(
+            <h1>Loading...</h1>
+            )}
+
+      </div>
 
       </div>
     );
   }
-  
   export default Dinner;
   
--- src/pages/Lunch.js
+++ src/pages/Lunch.js
@@ -1,12 +1,54 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import "../styling/Nutrition.css"
 
 function Lunch() {
+
+  const [allLunch, setAllLunch] = useState([])
+
+  const URL = "https://fitfusion.herokuapp.com/api/v1/lunch/"
+  
+  
+    const getAllLunch = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log("lunch data:", data.data);
+      setAllLunch(data.data);
+    };
+  
+    useEffect(() => {
+      getAllLunch();
+      // eslint-disable-next-line
+    }, []);
+
+
+
     return (
-      <div className="home-container">
+      <div className="recipes-flex-container">
         <h1> Lunch page</h1>
+        <div className="grid-container-nutrition">
+        {allLunch ? (
+          allLunch.map((lunch) => (
+            <div key={lunch._id} className="recipes">
+
+            <div className="nutr-box">
+            <Link to={`/lunch/show/${lunch._id}`}>
+              <img className="img-recipes nutr-images" src={lunch.img} alt=""></img>
+            </Link>
+              <h2 className="recipe-titles">{lunch.title}</h2>
+          </div>
+          </div>
+
+          ))
+          ):(
+            <h1>Loading...</h1>
+            )}
+
+      </div>
 
       </div>
     );
   }
-  
   export default Lunch;
   
