68aa1e11b5a32720d4e6730c9624b22848065006
Alina232323 <alina.polianska@vergesense.com>
2023-06-15 19:59:00 -0700

    added header to the main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
+import "../styling/Home.css"
 
 function Home() {
     return (
       <div className="home-container">
-        <h1> Home page</h1>
+        <img className="header-image-home" src="https://fitfusion.s3.us-west-1.amazonaws.com/Screenshot+2023-06-15+at+6.42.21+PM.png" alt=""></img>
+          <div className="headers-container">
+            <h1 className="home-header1">FITFUSION</h1>
+              <h2 className="home-header2">Empowering Welness</h2>
+            <button className="button-styled btn-h">SIGN UP</button>
+          </div>
 
       </div>
     );
